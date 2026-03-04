3227fdabad64e68d44fb5e7424592ee50d423cdd
ilonaSag <kupoles09@gmail.com>
2026-03-04 15:47:26 +0500

    Add files via upload

---
+++ ShapeTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using GeometryShape;
+namespace ShapeTests
+{
+    public class UnitTest1
+    {
+        //проверка прямоугольника
+        [Fact]
+        public void Rectangle_Area()
+        {
+            Rectangle rec = new Rectangle(3,3);
+        }
+    }
+}
